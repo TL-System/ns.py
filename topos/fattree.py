4d7ef17fef369093803041c5b0a5f856dba87e0b
Li Chen <li-ch@users.noreply.github.com>
2021-06-14 21:16:24 +0800

    Sync topology implementation

---
+++ topos/fattree.py
@@ -0,0 +1,82 @@
+import networkx as nx
+# based on FNSS datacenter topology implementation
+
+def build(k):
+    """
+    Return a fat tree datacenter topology, as described in [1]_
+
+    A fat tree topology built using k-port switches can support up to
+    :math:`(k^3)/4` hosts. This topology comprises k pods with two layers of
+    :math:`k/2` switches each. In each pod, each aggregation switch is
+    connected to all the :math:`k/2` edge switches and each edge switch is
+    connected to :math:`k/2` hosts. There are :math:`(k/2)^2` core switches,
+    each of them connected to one aggregation switch per pod.
+
+    Each node has three attributes:
+     * type: can either be *switch* or *host*
+     * tier: can either be *core*, *aggregation*, *edge* or *leaf*. Nodes in
+     * pod: the pod id in which the node is located, unless it is a core switch
+       the leaf tier are only host, while all core, aggregation and edge
+       nodes are switches.
+
+    Each edge has an attribute type as well which can either be *core_edge* if
+    it connects a core and an aggregation switch, *aggregation_edge*, if it
+    connects an aggregation and a core switch or *edge_leaf* if it connects an
+    edge switch to a host.
+
+    Parameters
+    ----------
+    k : int
+        The number of ports of the switches
+
+    Returns
+    -------
+    topology : netowrkx graph
+
+    References
+    ----------
+    .. [1] M. Al-Fares, A. Loukissas, and A. Vahdat. A scalable, commodity
+       data center network architecture. Proceedings of the ACM SIGCOMM 2008
+       conference on Data communication (SIGCOMM '08). ACM, New York, NY, USA
+       http://doi.acm.org/10.1145/1402958.1402967
+    """
+    # validate input arguments
+    if not isinstance(k, int):
+        raise TypeError('k argument must be of int type')
+    if k < 1 or k % 2 == 1:
+        raise ValueError('k must be a positive even integer')
+
+    topo = nx.Graph()
+    topo.name = "fat_tree_topology(%d)" % (k)
+
+    # Create core nodes
+    n_core = (k // 2) ** 2
+    topo.add_nodes_from([v for v in range(int(n_core))],
+                        layer='core', type='switch')
+
+    # Create aggregation and edge nodes and connect them
+    for pod in range(k):
+        aggr_start_node = topo.number_of_nodes()
+        aggr_end_node = aggr_start_node + k // 2
+        edge_start_node = aggr_end_node
+        edge_end_node = edge_start_node + k // 2
+        aggr_nodes = range(aggr_start_node, aggr_end_node)
+        edge_nodes = range(edge_start_node, edge_end_node)
+        topo.add_nodes_from(aggr_nodes, layer='aggregation',
+                            type='switch', pod=pod)
+        topo.add_nodes_from(edge_nodes, layer='edge', type='switch', pod=pod)
+        topo.add_edges_from([(u, v) for u in aggr_nodes for v in edge_nodes],
+                            type='aggregation_edge')
+    # Connect core switches to aggregation switches
+    for core_node in range(n_core):
+        for pod in range(k):
+            aggr_node = n_core + (core_node // (k // 2)) + (k * pod)
+            topo.add_edge(core_node, aggr_node, type='core_aggregation')
+    # Create hosts and connect them to edge switches
+    for u in [v for v in topo.nodes() if topo.node[v]['layer'] == 'edge']:
+        leaf_nodes = range(topo.number_of_nodes(),
+                           topo.number_of_nodes() + k // 2)
+        topo.add_nodes_from(leaf_nodes, layer='leaf', type='host',
+                            pod=topo.node[u]['pod'])
+        topo.add_edges_from([(u, v) for v in leaf_nodes], type='edge_leaf')
+    return topo
---
+++ topos/utils.py
@@ -0,0 +1,45 @@
+from random import sample
+import networkx as nx
+
+from flow.flow import Flow
+
+
+def read_topo(fname):
+  ftype = ".graphml"
+  if fname.endswith(ftype):
+    return nx.read_graphml(fname)
+  else:
+    print(f"{fname} is not GraphML")
+  
+def generate_flows(G, hosts, nflows):
+  all_flows = dict()
+  for flow_id in range(nflows):
+    src, dst = sample(hosts, 2)
+    all_flows[flow_id] = Flow(flow_id, src, dst)
+    all_flows[flow_id].path = sample(list(nx.all_simple_paths(
+      G, src, dst, cutoff=nx.diameter(G))), 1)[0]
+  return all_flows
+
+def generate_fib(G, all_flows):
+  for n in G.nodes():
+    node = G.nodes[n]
+
+    node['port_to_nexthop'] = dict()
+    node['nexthop_to_port'] = dict()
+
+    for port, nh in enumerate(nx.neighbors(G, n)):
+      node['nexthop_to_port'][nh] = port
+      node['port_to_nexthop'][port] = nh
+
+    node['flow_to_port'] = dict()
+    node['flow_to_nexthop'] = dict()
+
+  for f in all_flows:
+    flow = all_flows[f]
+    path = list(zip(flow.path, flow.path[1:]))
+    for seg in path:
+      a, z = seg
+      G.nodes[a]['flow_to_port'][flow.fid] = G.nodes[a]['nexthop_to_port'][z]
+      G.nodes[a]['flow_to_nexthop'][flow.fid] = z
+
+  return G
