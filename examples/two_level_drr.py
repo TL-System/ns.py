095ac1e1888c62974ce0a3bedaec78a8c5cfdad3
Baochun Li <bli@ece.toronto.edu>
2021-07-12 17:51:55 +0800

    Revised the two_level_drr example to fix an issue.

--- examples/two_level_drr.py
+++ examples/two_level_drr.py
@@ -13,7 +13,7 @@ from ns.scheduler.drr import DRRServer
 
 
 def packet_arrival():
-    return 1.75
+    return 1
 
 
 def const_size():
@@ -22,9 +22,11 @@ def const_size():
 
 env = simpy.Environment()
 
-total_groups = 3
-total_flows_per_group = 8
+total_groups = 2
+total_flows_per_group = 3
 source_rate = 8.0 * const_size() / packet_arrival()
+service_rate_L1 = total_groups * total_flows_per_group * source_rate
+service_rate_L2 = total_flows_per_group * source_rate
 
 group_weights = {}
 drr_server_per_group = {}
@@ -34,7 +36,7 @@ for grp_id in range(total_groups):
         group_weights[f'grp_{grp_id}_flow_{flow_id}'] = 1
 
 ps = PacketSink(env)
-drr_server = DRRServer(env, source_rate, group_weights, debug=True)
+drr_server = DRRServer(env, service_rate_L1, group_weights, zero_buffer=True)
 drr_server.out = ps
 
 # Setting up the DRR server for each group
@@ -43,11 +45,12 @@ for grp_id in range(total_groups):
     for flow_id in range(total_flows_per_group):
         flow_weights[f'grp_{grp_id}_flow_{flow_id}'] = 1
 
-    drr_server_per_group[f'grp_{grp_id}'] = DRRServer(env,
-                                                      source_rate,
-                                                      flow_weights,
-                                                      zero_buffer=True,
-                                                      debug=True)
+    drr_server_per_group[f'grp_{grp_id}'] = DRRServer(
+        env,
+        service_rate_L2,
+        flow_weights,
+        zero_buffer=True,
+        zero_downstream_buffer=True)
 
     for flow_id in range(total_flows_per_group):
         pg = DistPacketGenerator(env,
@@ -59,8 +62,9 @@ for grp_id in range(total_groups):
                                  flow_id=f"grp_{grp_id}_flow_{flow_id}")
         tail_drop_buffer = Port(env,
                                 source_rate,
-                                qlimit=8,
-                                zero_downstream_buffer=True)
+                                qlimit=None,
+                                zero_downstream_buffer=True,
+                                debug=True)
 
         pg.out = tail_drop_buffer
         tail_drop_buffer.out = drr_server_per_group[f'grp_{grp_id}']
