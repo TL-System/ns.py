670e94058f065ba3f1c165442187559c08eed04d
pancypeng <pancypeng@users.noreply.github.com>
2021-11-25 01:23:39 +0800

    add two new traffic generators
    Generator to yield inter-arrival times of two useful models: 1) Pareto on-off ; 2) BMAP (and of course MAP)

---
+++ examples/pipeline/MAP_MSP_trace_generator.py
@@ -0,0 +1,141 @@
+import numpy as np
+from numpy.random import rand
+
+PRECISION_VALUE = 1e-5
+
+# def check_BMAP_representation():
+
+
+def solve_CTMC(Q):
+    '''
+    Solve stationary distribution vector x for a CTMC with generator matrix Q 
+    balance equation: x * Q = 0 
+
+    Parameters
+    ----------
+    Q: generator matrix of a CTMC, row sum == 0
+
+    Return
+    -------
+    x.T: 1-D row vector, stationary distribution vector
+    '''
+
+    if np.any(abs(np.sum(Q, 1) - 0.0) > PRECISION_VALUE):
+        raise ValueError('Invalid CTMC Q matrix: Row sum not equal to 0')
+    A = Q.copy()
+    A[:, 0] = np.ones(A.shape[0])
+    b = np.zeros((A.shape[0], 1))
+    b[0] = 1.0
+    x = np.linalg.solve(A.T, b)
+
+    return x.T
+
+
+def solve_DTMC(P):
+    '''
+    '''
+    P = np.asarray(P)
+    if np.any(np.sum(P, 1) - 1.0 > PRECISION_VALUE):
+        raise ValueError('Invalid DTMC P matrix: Row sum not equal to 1')
+    return solve_CTMC(P - np.eye(P.shape[0]))
+
+
+def sum_matrix_list(mat_list):
+    sum_mat = np.zeros(mat_list[0].shape)
+    for mat in mat_list:
+        sum_mat += mat
+    return sum_mat
+
+
+def check_BMAP_representation(D_list, prec=PRECISION_VALUE):
+    ''''
+    '''
+    if len(D_list) == 2:
+        print('Input: MAP representation')
+    elif len(D_list) > 2:
+        print('Input: BMAP representation')
+    else:
+        print('neither MAP or BMAP representation')
+        return False
+
+    D0 = D_list[0]
+    for Dk in D_list[1:]:
+        if D0.shape != Dk.shape:
+            print('D0 and Dk have different shapes')
+            return False
+    for Dk in D_list[1:]:
+        if np.min(Dk) < -prec:
+            print('Dk has negative entry')
+            return False
+
+    if np.any(np.abs(np.sum(sum_matrix_list(D_list), 1)) > prec):
+        print('row sum is not zero')
+        return False
+
+    return True
+
+
+def BMAP_generator(D_list, initial=None, prec=PRECISION_VALUE):
+    '''
+    Generates random samples from a batch Markovian 
+    arrival process.
+    
+    Parameters
+    ----------
+    D_list : list of matrices of shape(M,M), length(N)
+        The D0...DN matrices of the BMAP
+    num_samples : integer
+        The number of samples to generate.
+    prec : double, optional
+        Numerical precision to check if the input BMAP is
+        valid. 
+    
+    Yield:
+    -------
+    x : one sample. 
+        if BMAP: list consisting of two values: the inter-arrival time and the type of the
+        arrival.   
+        if MAP: float, inter-arrival time     
+    '''
+
+    if not check_BMAP_representation(D_list):
+        raise ValueError(
+            "Samples From BMAP: Input is not a valid BMAP representation!")
+
+    M = D_list[0].shape[0]
+
+    if initial == None:
+        # draw initial state according to the stationary distribution
+        stat_distr_vec = solve_CTMC(sum_matrix_list(D_list))
+        cumm_initial = np.cumsum(stat_distr_vec)
+        r = rand()
+        state = 0
+        while cumm_initial[state] <= r:
+            state += 1
+    else:
+        state = initial
+
+    # auxilary variables
+    sojourn = -1.0 / np.diag(D_list[0])
+    nextpr = np.diag(sojourn) @ D_list[0]
+    nextpr = nextpr - np.diag(np.diag(nextpr))
+    for Dk in D_list[1:]:
+        nextpr = np.hstack((nextpr, np.diag(sojourn) * Dk))
+    nextpr = np.cumsum(nextpr, 1)
+
+    while True:
+        iat = 0
+
+        # play state transitions
+        while state < M:
+            iat -= np.log(rand()) * sojourn[state]
+            r = rand()
+            nstate = 0
+            while nextpr[state, nstate] <= r:
+                nstate += 1
+            state = nstate
+        state = nstate % M
+        if len(D_list) > 2:
+            yield [iat, nstate // M]
+        else:
+            yield iat
---
+++ examples/pipeline/Pareto_on_off_trace_generator.py
@@ -0,0 +1,71 @@
+import numpy as np
+from random import random
+
+
+def paretovariate_generator(xmin=1e-3, alpha=2.0):
+    '''
+    Pareto distribution.
+    Parameters
+    ----------------
+    xmin:   positive real
+            scale parameter, support [xmin, +inf)
+    alpha:  positive real
+            shape parameter
+    
+    Returns
+    ----------------
+    random variable conforming with Pareto(xmin, alpha)
+
+    Note: 
+    mean = inf, if alpha <= 1
+    mean = alpha * xmin / (alpha - 1), if alpha > 1
+    '''
+
+    u = 1.0 - random()
+    return xmin / u**(1.0 / alpha)
+
+
+def pareto_onoff_generator(on_min=0.5 / 3,
+                           on_alpha=1.5,
+                           off_min=0.5 / 3,
+                           off_alpha=1.5,
+                           on_rate=2e5,
+                           pktsize=1000):
+    '''
+    Pareto on/off traffic generator
+    Packets are sent at fixed rate during on periods, and no packets are sent during off periods.
+    Both on and off periods are taken from a Pareto distribution with constant size packets.
+    Parameters
+    ----------------
+    on_min:   positive real
+            scale parameter, support [on_min, +inf)
+    on_alpha:  positive real
+            shape parameter
+    off_min:   positive real
+            scale parameter, support [off_min, +inf)
+    off_alpha:  positive real
+            shape parameter
+    
+    Yields
+    ----------------
+    current_iat : current interarrival time (sec)
+    
+    '''
+
+    #     burst_time = on_alpha * on_min / (on_alpha - 1)
+    #     idle_time = off_alpha * off_min / (off_alpha - 1)
+    #     burst_len = np.ceil(burst_time / interval)
+
+    interval = pktsize * 8 / on_rate
+    remain_pkts = 0
+    while True:
+        if remain_pkts == 0:
+            next_burstlen = np.ceil(
+                paretovariate_generator(on_min, on_alpha) + 0.5)
+            remain_pkts = next_burstlen
+            next_idle_time = paretovariate_generator(off_min, off_alpha)
+            current_iat = next_idle_time
+        else:
+            remain_pkts -= 1
+            current_iat = interval
+        yield current_iat
